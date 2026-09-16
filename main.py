2be799b66735fd56a723547b9d3ab364b0a31913
AndrewBezotchestvo <96548028+AndrewBezotchestvo@users.noreply.github.com>
2026-09-16 14:18:42 +0800

    add

---
+++ main.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, IntegerField, EmailField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db' #указываем путь для БД
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False #отключение модификаторов отслеживания БД
+app.config['SECRET_KEY'] = 'secret'
+
+db = SQLAlchemy(app) #объявление бд
+
+class Note(db.Model): #создание таблицы
+    id = db.Column(db.Integer, primary_key=True) #создание полей
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+class NoteForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = StringField('Note Text', validators=[DataRequired()])
+    submit = SubmitField('Save')
+
+# ---- Добавление ----
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = NoteForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        note = Note(title=title, content=content) #создание объекта для БД
+        db.session.add(note) #добавление эл. в БД
+        db.session.commit() #сохранение эл. в БД
+        return redirect("/")
+    return render_template("add.html", form=form)
+
+@app.route('/')
+def index():
+    notes = Note.query.all() #считать все данные с БД
+    return render_template("main.html", notes=notes)
+
+# ---- Просмотр ----
+@app.route('/note/<int:id>',)
+def note(id):
+    note = Note.query.get(id) #получить информацию по эл базы данных
+    return render_template("note.html", note=note)
+
+# ---- Удаление ----
+@app.route('/delete-note/<int:id>')
+def delete_note(id):
+    note = Note.query.get(id) #получить заметку
+    db.session.delete(note)  #удалить заметку
+    db.session.commit() #сохранить изменения
+    return redirect("/") # вернуться на главную
+
+if __name__ == '__main__':
+    with app.app_context(): #открытие всех файлов приложения
+        db.create_all() #создание файла БД
+    app.run(debug=True)
+
+
+
